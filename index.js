7878ddb3b95ede36a47584bf72c8ea15516e49ae
manasigithub <manikumarmanasi16@gmail.com>
2022-10-04 15:38:14 +0530

    operators

---
+++ index.js
@@ -0,0 +1,89 @@
+//Arithmatic logic
+let a=20;
+let b=10;
+let c=a+b;
+let c1=a-b;
+let c2=a*b;
+let c3=a/b;
+let c4=a%b;
+console.log(c);
+console.log(c1);
+console.log(c2);
+console.log(c3);
+console.log(c4);
+
+//Relational logic
+let a1=15;
+let a2=20;
+let r = a<b;
+console.log(r);
+
+let r1=a>b;
+console.log(r1);
+
+const num=20;
+const num1=50;
+const result = num>=num1;
+console.log(result);
+
+const result1 = num<=num1;
+console.log(result1);
+
+var x=2;
+var y=2;
+var r3= x==y;
+console.log(r3);
+
+var x=2;
+var y=2;
+var r3= x!=y;
+console.log(r3);
+
+//Logical operator
+let aa=5;
+let bb=15;
+let rr=(aa<10 || bb>10);
+let rr1=(aa>10 && bb<10);
+console.log(rr);
+console.log(rr1);
+
+//Assignment operator
+let s1=4;
+let s2=6;
+console.log(s1=s2);
+console.log(s2=s1);
+console.log(s1+=s2);
+console.log(s1-=s2);
+console.log(s1*=s2);
+console.log(s1/=s2);
+
+
+//Increment & decrement operator
+var i=2;
+var j=4;
+console.log(i++);
+console.log(++i);
+console.log(i--);
+console.log(--i);
+console.log(j++);
+console.log(++j);
+console.log(j--);
+console.log(--j);
+
+//Conditional operator
+let sum = 10;
+let sum1 = 20;
+console.log(sum1>sum ? 100 :200);
+
+//Bitwise operator
+let ss=10;
+let ss1=20;
+console.log(ss & ss1);
+console.log(ss | ss1);
+console.log(ss ^ ss1);
+
+//Special operator
+var xx=5;
+var yy=8;
+console.log("x=",xx,"y=",yy)
+
